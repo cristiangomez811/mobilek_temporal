2963e3c520b0602e04527af5db7d12d1c3b28aee
cristiangomez811 <cristian.gomezr@upb.edu.co>
2017-07-21 19:50:28 -0500

    primer formulario
    se hizo un formulario para clientes, aun flata el funcionamiento en php

---
+++ clienteController.php
@@ -0,0 +1,9 @@
+<?php
+  require_once(dbConection.php);
+
+  $nombre = $_POST['nombre'];
+  $cedula = $_POST['cedula'];
+  $telefono = $_POST['telefono'];
+
+  echo "se guardo el cliente ";
+?>
---
+++ dbConection.php
@@ -0,0 +1,16 @@
+<?php
+  // conexion a la base de datos
+  $mysqli = new mysqli("localhost", "root", "", "mobilek");
+  if ($mysqli->connect_errno)
+   {
+    echo "Falló la conexión con MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+   }
+
+   public function Create($value)
+   {
+     # code...
+     return;
+   }
+
+
+ ?>
